a8fe9eb9b0cc7127953279176d4ceb351d282301
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-02-27 03:14:08 +0100

    covid response

---
+++ covid-19.php
@@ -0,0 +1,31 @@
+<?php
+
+require './setup.php';
+
+use Utility\Functions;
+
+$navbar = null;
+const TITLE = 'COVID-19 Response';
+
+Functions::extend('header');
+?>
+
+<main id="main">
+  <section>
+    <div class="container-fluid">
+
+      <h1 class="section-title text-dark">Our COVID experience</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus odio molestiae expedita minus nobis, numquam accusamus at aut repellat dolore, nisi illo quaerat aperiam error! Exercitationem, hic. Quod, nostrum aperiam.
+        Nam consectetur maiores similique odit distinctio, soluta sint suscipit, tempore quasi quia ut sapiente ullam aspernatur esse fugit voluptatibus consequatur dicta vero itaque animi minima accusamus perspiciatis! Esse, minima asperiores?
+        Magni iusto libero esse maxime est accusamus veritatis nesciunt at quidem enim molestiae numquam quos cum expedita molestias, doloremque nisi totam voluptates? Recusandae nisi, dolore cum quasi quas nemo. Adipisci!
+        Placeat praesentium ratione ab vel, harum quas accusantium illo at? Laborum omnis consectetur hic magni, harum vel necessitatibus doloremque tempore ratione temporibus rem delectus, aliquid labore ullam est a deserunt.
+        Repellat minima, voluptatem officia fugiat cumque ratione reprehenderit est, nulla veritatis ipsa corrupti ab possimus, nostrum dolorem architecto nobis! Harum delectus labore quae cumque optio ex odio, obcaecati facere aut.
+        Veritatis blanditiis quo perspiciatis minus itaque deleniti, optio vero reiciendis, impedit harum et tenetur molestiae laborum, officia adipisci provident. Aperiam, reiciendis? Sit et placeat laboriosam odit nostrum in cum quo.
+        Provident dignissimos minima exercitationem nobis dolorum sequi consectetur! Cupiditate ad esse totam nemo rem. Nam, laudantium ullam? Quos eaque deserunt nostrum iure, delectus facilis eligendi beatae? Quia, quisquam possimus! Necessitatibus!
+        Accusantium eos eaque optio illo, voluptatibus laudantium dolorem voluptate recusandae culpa explicabo pariatur fuga quae consequuntur? Vitae voluptatum, veritatis sapiente aliquid culpa cupiditate, voluptate perferendis ad atque libero rerum eveniet.
+        Aliquid temporibus doloremque perferendis laudantium, hic obcaecati facere dolore pariatur natus praesentium voluptatum nobis repudiandae et quas, dicta asperiores reiciendis modi tempore. Sapiente consequatur rem laborum saepe delectus nostrum dignissimos.
+        Aut quas beatae quisquam corrupti ducimus ipsa. Molestias laborum numquam laudantium consectetur doloremque reiciendis dolores voluptate ut! Dolorum, iure maxime, facere adipisci, tenetur cum et ex ipsum nemo sit ratione!</p>
+    </div>
+
+  </section>
+</main>
