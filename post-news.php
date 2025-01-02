c18681a4e6937297c099a1290f303aeba79b7e0b
Boluwatife <adeojopeter@gmail.com>
2025-01-02 10:43:58 +0100

    responsiveness

--- post-news.php
+++ post-news.php
@@ -45,30 +45,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 Functions::extend('header');
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Summernote</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"></script>
-</head> -->
 
 <main class="py-3">
     <div class="container">
         <form action="" method="post">
             <div class="row pb-3">
-                <div class="col-3">
+                <div class="col-md-3 col-sm-12">
                     <div class="form-group">
                         <label for="">Username</label>
                         <input type="text" name="username" required="required" class="form-control" />
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-md-3 col-sm-12">
                     <div class="form-group">
                         <label for="">Password</label>
                         <input type="password" name="password" required="required" class="form-control" />
@@ -77,7 +65,7 @@ Functions::extend('header');
             </div>
 
             <div class="row pb-3">
-                <div class="col-6">
+                <div class="col-md-6">
                     <div class="form-group">
                         <label for="">Title</label>
                         <input type="text" name="title" required="required" class="form-control" />
